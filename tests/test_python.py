bc13469e5d91a77aa48030ad565c12a37e6dc7d5
discopatrick <git@discopatrick.me>
2016-11-07 12:14:38 +0000

    class instantiation test

--- tests/test_python.py
+++ tests/test_python.py
@@ -1,5 +1,6 @@
 from unittest import TestCase
 from src.functions import *
+from src.classes import *
 
 class FunctionTest(TestCase):
 
@@ -8,3 +9,11 @@ class FunctionTest(TestCase):
         val = return_a_value()
 
         self.assertIsNotNone(val)
+
+class ClassTest(TestCase):
+
+    def test_class_can_be_instantiated(self):
+
+        my_object = Person()
+
+        self.assertIsInstance(my_object, Person)
